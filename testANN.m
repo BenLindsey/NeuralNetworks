aaa5c7c01e308a188a5109ecd5d89331d6b73ca9
Christophe Steininger <christophe.steininger12@imperial.ac.uk>
2014-11-06 14:56:37 +0000

    Implement the testANN function.

--- testANN.m
+++ testANN.m
@@ -1,4 +1,7 @@
-function [ predictions ] = testANN( net, x2 )
-
+function predictions = testANN( net, x2 )
+    % Run the given network with the input data.
+    output = sim(net, x2);
+    
+    % Only output the maximum value in each row.
+    predictions = NNout2labels(output);
 end
-
