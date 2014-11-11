07a8178c0fd2f73c413f50c267812d38e7afb4da
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-11-11 11:37:35 +0000

    FUCK MY LIFE

---
+++ ga_optimise_gda.m
@@ -0,0 +1,29 @@
+function [ net ] = ga_optimise_gda(trainingInput, trainingOutput, validatingInput, validatingOutput) 
+    % OPTIMISATION ARGUMENTS:
+    % [ NeuronsInFirstLayer, NeuronsInSecondLayer ]
+    nargs = 2;    % Number of arguments
+    LB = [1 0];   % Lower bounds for arguments
+    UB = [10 10]; % Upper bounds for arguments
+    IC = [1, 2];  % Integer constraints (by index of OPTIMISATION ARGUMENTS)
+    %options = gaoptimset; % Default options
+    options = gaoptimset('UseParallel', true, 'Vectorized', 'off'); % Timelimit(seconds) constraint
+                                          % ONLY checked after 1st gen.
+                                         
+    % Training data
+    [tI, tO] = ANNdata(trainingInput, trainingOutput);  
+    
+    % Calculate fit for the given optimisation arguments. Lower is better.
+    FitnessFunction = @(args) nn_fitness_gda(args, ...
+        tI, tO, validatingInput, validatingOutput);
+    
+    % Result of genetic algorithm search, where x is discovered values
+    % for OPTIMISATION ARGUMENTS
+    [x, fval] = ga(FitnessFunction, nargs, [], [], [], [], ...
+                    LB, UB, [], IC, options);
+    disp('Using values:');
+    disp(x);
+    net = feedforwardnet([x(1), x(2)]);
+    net = configure(net, tI, tO);
+    net = train(net, tI, tO);
+end
+
---
+++ nn_fitness_gda.m
@@ -0,0 +1,22 @@
+function [ fit ] = nn_fitness_gda( args, trainingInput, trainingOutput, validatingInput, validatingOutput )
+    if length(args) > 1 && args(2) > 0
+        net = feedforwardnet([args(1), args(2)], 'traingda');
+    else
+        net = feedforwardnet([args(1)], 'traingda');
+    end
+    
+    net = configure(net, trainingInput, trainingOutput);
+    
+    net = train(net, trainingInput, trainingOutput);
+    
+    matrix = confusionmatrix();
+    matrix.update(net, validatingInput, validatingOutput);
+    fit = 1 - matrix.getAccuracy();
+
+    if length(args) > 1
+        disp([num2str(args(1)), ' ', num2str(args(2)), ' -> ', num2str(fit)]);
+    else
+        disp([num2str(args(1)), ' -> ', num2str(fit)]);
+    end
+end
+
