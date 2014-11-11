017a535b61bacbb2fa2d1a984a4be985298bddf0
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-11-11 11:54:58 +0000

    added params to gda

--- nn_fitness_gda.m
+++ nn_fitness_gda.m
@@ -1,5 +1,5 @@
 function [ fit ] = nn_fitness_gda( args, trainingInput, trainingOutput, validatingInput, validatingOutput )
-    if length(args) > 1 && args(2) > 0
+    if args(2) > 0
         net = feedforwardnet([args(1), args(2)], 'traingda');
     else
         net = feedforwardnet([args(1)], 'traingda');
@@ -7,16 +7,18 @@ function [ fit ] = nn_fitness_gda( args, trainingInput, trainingOutput, validati
     
     net = configure(net, trainingInput, trainingOutput);
     
+    net.trainParam.lr     = args(3);
+    net.trainParam.lr_inc = args(4);
+    net.trainParam.lr_dec = args(5);
+    
     net = train(net, trainingInput, trainingOutput);
     
     matrix = confusionmatrix();
     matrix.update(net, validatingInput, validatingOutput);
     fit = 1 - matrix.getAccuracy();
 
-    if length(args) > 1
-        disp([num2str(args(1)), ' ', num2str(args(2)), ' -> ', num2str(fit)]);
-    else
-        disp([num2str(args(1)), ' -> ', num2str(fit)]);
-    end
+    disp(['Neurons:', num2str(args(1)), '|', num2str(args(2)), ...
+          ' Lr:', num2str(args(3)), ' Lr_inc:', num2str(args(4)), ...
+          ' Lr_dec:', num2str(args(5)),' -> ', num2str(fit)]);
 end
 
