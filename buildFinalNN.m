73620c66072f3c4ed2daa55570cc19b3b37b962c
Ben Lindsey <ben.lindsey12@imperial.ac.uk>
2014-11-16 18:56:06 +0000

    Fixed buildFinal (only tested with gda)

--- buildFinalNN.m
+++ buildFinalNN.m
@@ -1,20 +1,20 @@
 function [ net ] = buildFinalNN( gd_optiset, gda_optiset, gdm_optiset, rp_optiset, x, y)
-    [gda_accuracy, gda_params ] = findBestForTrainingFunction(gda_optiset, 'traingda', 6, @assign_gda, x, y)
     [gdm_accuracy, gdm_params ] = findBestForTrainingFunction(gdm_optiset, 'traingdm', 0, @assign_gdm, x, y)
     [rp_accuracy, rp_params ] = findBestForTrainingFunction(rp_optiset, 'trainrp', 5, @assign_rp, x, y)
-    [gd_accuracy, gd_params ] = findBestForTrainingFunction(gd_optiset, 'traingd', 3, @assign_gd, x, y)
+    [gd_accuracy, gd_params ] = findBestForTrainingFunction(gd_optiset, 'traingd', 4, @assign_gd, x, y)
+    [gda_accuracy, gda_params ] = findBestForTrainingFunction(gda_optiset, 'traingda', 6, @assign_gda, x, y)
     
     scores = [gda_accuracy, gdm_accuracy, rp_accuracy, gd_accuracy];
     best = max(scores(:));
     
     if best == gda_accuracy
-    	net = buildNet(x, y, gda_params, 6, 'traingda', @assign_gda);
+    	net = buildNet(1, x, y, gda_params, 6, 'traingda', @assign_gda);
     elseif best == gdm_accuracy
-        net = buildNet(x, y, gdm_params, 0, 'traingdm', @assign_gdm);
+        net = buildNet(1, x, y, gdm_params, 0, 'traingdm', @assign_gdm);
     elseif best == rp_accuracy
-        net = buildNet(x, y, rp_params, 5, 'trainrp', @assign_rp);
+        net = buildNet(1, x, y, rp_params, 5, 'trainrp', @assign_rp);
     else
-        net = buildNet(x, y, gd_params, 4, 'traingd', @assign_gd);
+        net = buildNet(1, x, y, gd_params, 4, 'traingd', @assign_gd);
     end
 end
 
@@ -42,7 +42,7 @@ function [ result ] = assign_gd( net, args )
     result = net;
 end
 
-function [ net ] = buildNet(input, output, args, layerIndex, functionName, argAssigner)
+function [ net ] = buildNet(i, input, output, args, layerIndex, functionName, argAssigner)
     rng(1001, 'twister');
     
     if layerIndex == 0 || args(layerIndex) > 0
@@ -55,6 +55,7 @@ function [ net ] = buildNet(input, output, args, layerIndex, functionName, argAs
     valInd = i:10:size(tI, 2);
     trainInd = (1:size(tI, 2));
     trainInd(valInd) = [];
+    
     net = configure(net, tI, tO);
     net.divideFcn = 'divideind';
     net.divideParam.trainInd = trainInd;
@@ -69,6 +70,14 @@ function [ net ] = buildNet(input, output, args, layerIndex, functionName, argAs
 end
 
 function [ max_accuracy, max_inputs ] = findBestForTrainingFunction( optiset, functionName, layerIndex, argAssigner, x, y )
+    max_accuracy = 0;
+    max_inputs = [];
+    
+    if isempty(optiset)
+        return
+    end
+
+
     for j=1:10
         confusedMatrix = confusionmatrix();
 
@@ -86,7 +95,7 @@ function [ max_accuracy, max_inputs ] = findBestForTrainingFunction( optiset, fu
 
             args = optiset(j, :);
 
-            net = buildNet(foldInput, foldOutput, args, layerIndex, functionName, argAssigner);
+            net = buildNet(i, foldInput, foldOutput, args, layerIndex, functionName, argAssigner);
             
             % Update the confusion matrix with the test data for this fold.
             confusedMatrix.update(net, testInput, testOutput);
